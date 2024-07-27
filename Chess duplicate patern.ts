47fbf1c6ede9f0aefd2f1c0e8e0badb74184ab8f
Bruno Duyé <brunetton@brunetton.org>
2024-07-27 15:55:24 +0200

    Add spacing and margins options

--- Chess duplicate patern.ts
+++ Chess duplicate patern.ts
@@ -11,6 +11,7 @@ export enum BoolOpts {
     annotation { "Name" : "Intersect" } INTERSECT,
 }
 
+
 annotation { "Feature Type Name" : "Chess duplicate patern", "Feature Type Description" : "Duplicate selected part using to create a chess board like pattern." }
 export const chessPattern = defineFeature(function(context is Context, id is Id, definition is map)
     precondition
@@ -34,11 +35,28 @@ export const chessPattern = defineFeature(function(context is Context, id is Id,
         isInteger(definition.cols, POSITIVE_COUNT_BOUNDS);
         annotation { "Name" : "Flip", "UIHint" : UIHint.OPPOSITE_DIRECTION }
         definition.rowsFlipped is boolean;
-        annotation { "Name" : "X spacing", "UIHint" : "REMEMBER_PREVIOUS_VALUE" }
-        isLength(definition.xSpacing, { (millimeter) : [-100000000, 0, 100000000] } as LengthBoundSpec);
-        annotation { "Name" : "Y spacing", "UIHint" : "REMEMBER_PREVIOUS_VALUE" }
-        isLength(definition.ySpacing, { (millimeter) : [-100000000, 0, 100000000] } as LengthBoundSpec);
+        // MANUAL SPACING
+        annotation { "Name" : "Manual spacing" }
+        definition.manualSpacing is boolean;
+        if (definition.manualSpacing) {
+            annotation { "Group Name" : "Options", "Collapsed By Default" : false, "Driving Parameter" : "manualSpacing" } {
+                annotation { "Name" : "X spacing", "UIHint" : "REMEMBER_PREVIOUS_VALUE" }
+                isLength(definition.xSpacing, { (millimeter) : [-100000000, 0, 100000000] } as LengthBoundSpec);
+                annotation { "Name" : "Y spacing", "UIHint" : "REMEMBER_PREVIOUS_VALUE" }
+                isLength(definition.ySpacing, { (millimeter) : [-100000000, 0, 100000000] } as LengthBoundSpec);
+            }
+        }
+
+        // MARGINS
+        annotation { "Group Name" : "Margins", "Collapsed By Default" : true } {
+            annotation { "Name" : "Border X margin", "UIHint" : "REMEMBER_PREVIOUS_VALUE"}
+            isLength(definition.borderXMargin, { (millimeter) : [-100000000, 0, 100000000] } as LengthBoundSpec);
+            annotation { "Name" : "Border Y margin", "UIHint" : "REMEMBER_PREVIOUS_VALUE"}
+            isLength(definition.borderYMargin, { (millimeter) : [-100000000, 0, 100000000] } as LengthBoundSpec);
+        }
+
     }
+
 
     {
         // Iterate through rows
@@ -55,6 +73,13 @@ export const chessPattern = defineFeature(function(context is Context, id is Id,
         var colsDirection = -1;
         if (definition.rowsFlipped) rowsDirection = -1;
         if (definition.colsFlipped) colsDirection = 1;
+        // parse spacing options
+        var xSpacing = 0 * inch;
+        var ySpacing = 0 * inch;
+        if (definition.manualSpacing) {
+            xSpacing = definition.xSpacing;
+            ySpacing = definition.ySpacing;
+        }
         var transforms = [];
         var names = [];
         // Prepare transforms and names arrrays that will be passed to opPattern()
@@ -64,18 +89,16 @@ export const chessPattern = defineFeature(function(context is Context, id is Id,
 
             // Iterate through columns with offset for odd rows
             for (var col = colOffset; col < definition.cols; col += 2) {
-
                 names = append(names, id[0] ~ '-' ~ i);
                 // debug(context, "row-col: " ~ row ~ "-" ~ col);
                 transforms = append(transforms, transform(vector(
-                    rowsDirection * row * (entity_size[0] + definition.xSpacing),
-                    colsDirection * col * (entity_size[1] + definition.ySpacing),
+                    rowsDirection * row * (entity_size[0] + xSpacing) + definition.borderXMargin,
+                    colsDirection * col * (entity_size[1] + ySpacing) + definition.borderYMargin,
                     0*inch
                 )));
                 i += 1;
             }
         }
-
         // call opPattern() (create copies)
         var featureId = id + "pattern";
         opPattern(context, featureId, {
